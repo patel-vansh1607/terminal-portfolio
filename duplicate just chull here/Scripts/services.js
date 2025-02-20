2a7ad9a0d7eb0781eafbb7d426997d62599995cf
Vansh Patel <patel.vansh1607@gmail.com>
2025-02-20 23:05:42 +0300

    added

---
+++ duplicate just chull here/Scripts/services.js
@@ -0,0 +1,8 @@
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = 'flex';
+  }
+  
+  function closePopup(popupId) {
+    document.getElementById(popupId).style.display = 'none';
+  }
+  
