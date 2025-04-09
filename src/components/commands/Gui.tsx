2aa6cbee9b46a2deae73a5323e651ffcb84ff3f6
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-09 12:28:12 +0300

    Update redirect URL in Gui component

--- src/components/commands/Gui.tsx
+++ src/components/commands/Gui.tsx
@@ -10,7 +10,7 @@ const Gui: React.FC = () => {
 
   /* ===== check current command makes redirect ===== */
   if (rerender && currentCommand[0] === "gui") {
-    window.open("https://satnaing.dev/", "_blank");
+    window.open("https://patelvansh1607pf.vercel.app/", "_blank");
   }
 
   return <span></span>;
