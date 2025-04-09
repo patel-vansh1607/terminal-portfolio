5666129ac18be75d9e07c97c6768c05a1f796437
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-09 12:27:57 +0300

    Update About component with personal details and interests

--- src/components/commands/About.tsx
+++ src/components/commands/About.tsx
@@ -8,15 +8,23 @@ const About: React.FC = () => {
   return (
     <AboutWrapper data-testid="about">
       <p>
-        Hi, my name is <HighlightSpan>Sat Naing</HighlightSpan>!
+        Hi, my name is <HighlightSpan>Vansh Patel</HighlightSpan>!
       </p>
       <p>
-        I'm <HighlightAlt>a full-stack developer</HighlightAlt> based in Yangon,
-        Myanmar.
+        I'm <HighlightAlt>a :
+           <ul>
+                <li>Full-Stack Engineering Student.</li>
+                <li>Video Editor.</li>
+                <li>UI/UX Designer. </li>
+                <li> Graphic Desinger.</li> 
+                <li>Videographer</li> 
+                <li>Photographer</li>
+            </ul>
+             </HighlightAlt> based in Nakuru,
+        Kenya.
       </p>
       <p>
-        I am passionate about writing codes and <br />
-        developing web applications to solve real-life challenges.
+        I love coding and designing, and I'm always looking for new opportunities to learn and grow with my code.
       </p>
     </AboutWrapper>
   );
